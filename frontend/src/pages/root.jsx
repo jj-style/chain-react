f98f73f88e8b59fb67f6a0252d290a17adff60e0
JJ Style <style.jj@pm.me>
2023-04-20 00:34:07 +0100

    add meilisearch in and custom searchbox

--- frontend/src/pages/root.jsx
+++ frontend/src/pages/root.jsx
@@ -1,9 +1,29 @@
 import withLayout from "./layout";
+import { InstantSearch, Hits, Highlight } from "react-instantsearch-dom";
+import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
+import { SearchBox } from "../components";
+
 const Root = () => {
+  const searchClient = instantMeiliSearch(
+    process.env.REACT_APP_MEILISEARCH_HOST,
+    process.env.REACT_APP_MEILISEARCH_API_KEY,
+    {
+      placeholderSearch: false,
+      primaryKey: "Id",
+    }
+  );
+
   return (
     <div id="root">
       <h1>Root</h1>
+      <InstantSearch indexName="actors" searchClient={searchClient}>
+        <SearchBox />
+        <Hits hitComponent={Hit} />
+      </InstantSearch>
     </div>
   );
 };
+
+const Hit = ({ hit }) => <Highlight attribute="Name" hit={hit} />;
+
 export default withLayout(Root);
