20ff22e43f40cdf283d4b931edef90433f6fad17
JJ Style <style.jj@pm.me>
2023-04-20 20:27:08 +0100

    add attribution to footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,10 +1,19 @@
 import MadeWithLove from "react-made-with-love";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import tmdbLogo from "../assets/tmdb.svg";
+
 const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 bg-light fixed-bottom">
-      <Container className="d-flex justify-content-center">
+      <Container className="d-flex justify-content-between">
+        <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
+          <Image src={tmdbLogo} width={48} />
+        </a>
         <MadeWithLove by="JJ Style" emoji link="https://github.com/jj-style" />
+        <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
+          <Image src={tmdbLogo} width={48} />
+        </a>
       </Container>
     </footer>
   );
